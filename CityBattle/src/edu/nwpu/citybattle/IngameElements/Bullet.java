97b872c821a8e3f9fbc3c140f6de37ad674cb3f4
Orangii <14857984+Jiaocz@users.noreply.github.com>
2020-12-27 19:31:12 +0800

    update version info

--- CityBattle/src/edu/nwpu/citybattle/IngameElements/Bullet.java
+++ CityBattle/src/edu/nwpu/citybattle/IngameElements/Bullet.java
@@ -11,38 +11,43 @@ import edu.nwpu.citybattle.alogrism.CronJobSet;
  * 本类为坦克子弹类，子弹对象实例化后会自动挂载到静态子弹列表中及计划任务中。
  * 
  * @author Orangii
- * @version 1.0.0
+ * @version 1.0.2
  * @see edu.nwpu.citybattle.alogrism.BulletAlogrism
  */
-public class Bullet implements Movable{
+public class Bullet implements Movable {
 	/**
 	 * 此{@code ArrayList}维护所有场上的子弹
+	 * 
 	 * @since 1.0.0
 	 * @see ArrayList
 	 */
 	public static final ArrayList<Bullet> Bullets = new ArrayList<Bullet>();
-	
+
 	/**
 	 * 子弹的X坐标
+	 * 
 	 * @since 1.0.0
 	 */
 	public int pos_x;
 	/**
 	 * 子弹的X坐标
+	 * 
 	 * @since 1.0.0
 	 */
 	public int pos_y;
 	/**
 	 * 子弹的飞行方向，使用构造函数设定
+	 * 
 	 * @since 1.0.0
 	 */
 	public final int direction;
-	
+
 	// 子弹速度
 	private static int speed = 1;
 
 	/**
 	 * 构造一个子弹，由其X、Y坐标及其方向
+	 * 
 	 * @param pos_x
 	 * @param pos_y
 	 * @param direction
@@ -52,7 +57,7 @@ public class Bullet implements Movable{
 		this.pos_x = pos_x;
 		this.pos_y = pos_y;
 		this.direction = direction;
-		// Add bullet to ArrayList 
+		// Add bullet to ArrayList
 		Bullets.add(this);
 		CronJobSet.addJob(this);
 	}
@@ -63,22 +68,22 @@ public class Bullet implements Movable{
 	@Override
 	public void moveNext() {
 		// 该变X、Y坐标
-		switch(this.direction) {
-			case UP:
-				this.pos_y -= Bullet.speed;
-				break;
-			case DOWN:
-				this.pos_y += Bullet.speed;
-				break;
-			case LEFT:
-				this.pos_x -= Bullet.speed;
-				break;
-			case RIGHT:
-				this.pos_x += Bullet.speed;
-				break;
+		switch (this.direction) {
+		case UP:
+			this.pos_y -= Bullet.speed;
+			break;
+		case DOWN:
+			this.pos_y += Bullet.speed;
+			break;
+		case LEFT:
+			this.pos_x -= Bullet.speed;
+			break;
+		case RIGHT:
+			this.pos_x += Bullet.speed;
+			break;
 		}
 	}
-	
+
 	/**
 	 * 静态方法，通过子弹对象在静态子弹列表中删除某子弹，同时删除其计划任务。
 	 * 
@@ -88,7 +93,7 @@ public class Bullet implements Movable{
 	public static boolean remove(Bullet bullet) {
 		return Bullets.remove(bullet) && CronJobSet.removeJob(bullet);
 	}
-	
+
 	/**
 	 * 通过子弹本身删除子弹及其计划任务
 	 * 
