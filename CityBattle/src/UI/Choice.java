40f08a60e4d7ff46d2c0579c82e45f1c241bb529
琚翔 <@DESKTOP-MLM638K>
2020-12-28 09:48:34 +0800

    UI界面第一次

---
+++ CityBattle/src/UI/Choice.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package UI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Dimension;
+
+/**
+ * @author 琚翔
+ * version V1.0
+ */
+public class Choice extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Choice frame = new Choice();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Choice() {
+		setBackground(new Color(0, 0, 0));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();//获得屏幕尺寸
+		int width = 1000;
+		int height = 620;//设置窗口宽度和高度
+		setBounds((d.width-width)/2, (d.height-height)/2, width, height);//窗口的坐标和尺寸，以此种方式居中
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
