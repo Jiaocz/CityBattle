1802d8126d49626c9885986577b553b1c09fa138
Aurora <Aurora@LX20201226>
2020-12-29 21:51:26 +0800

    gai

--- CityBattle/src/edu/nwpu/citybattle/IngameElements/AiTank.java
+++ CityBattle/src/edu/nwpu/citybattle/IngameElements/AiTank.java
@@ -11,48 +11,35 @@ import edu.nwpu.citybattle.actions.Movable;
  * @see Tank
  */
 public class AiTank extends Tank implements Movable {
-	/**
-	 * ��ʼ����̹�˵����꣬������̹�����������ɵ��µ�AiTank
-	 */
+
 	private int initX;
 	private int initY;
 
-	/**
-	 * ̹�˵��ٶ�
-	 */
-	private int speed = 5;
+	private int speed = 1;
 
-	/**
-	 * ̹�˵Ŀ���
-	 */
-	private int width = 40;
-	private int height = 40;
-
-	/**
-	 * AiTanks�ƶ�
-	 */
 	@Override
 	public void moveNext() {
-		// FIXME moveNext()方法是定时器调用，这里不要写while，会冲突
-		while (judgeLimit()) {
 
-			if (direction == 2) {
+		if (judgeLimit()) {
+
+			if (direction == DOWN) {
 				tank_y += speed;
 				setRandomDir();
 			}
-			if (direction == 1) {
+			if (direction == UP) {
 				tank_y -= speed;
 				setRandomDir();
 			}
-			if (direction == 3) {
+			if (direction == LEFT) {
 				tank_x -= speed;
 				setRandomDir();
 			}
-			if (direction == 4) {
+			if (direction == RIGHT) {
 				tank_x += speed;
 				setRandomDir();
 			}
-		}
+		} else
+			setRandomDir();
 	}
 
 	@Override
@@ -60,59 +47,52 @@ public class AiTank extends Tank implements Movable {
 		HP--;
 	}
 
-	/**
-	 * �ж��Ƿ�������
-	 * 
-	 * @return
-	 */
 	private boolean judgeLimit() {
-		
-		// FIXME 算法有问题，下课来我办公室
-		if (tank_x < 0) {
-			tank_x = 0;
+		int a;
+
+		if (tank_x <= 0 && tank_x >= 52) {
 			return true;
 		}
-		if (tank_y < 0) {
-			tank_y = 0;
+		if (tank_y <= 0 && tank_y >=36) {
 			return true;
 		}
 		switch (direction) {
 		case UP:
-			for(int i=0;i<5;i++){
-				for(int j=0;j<5;j++) {
-					if(Map.ironwall[tank_x+i][tank_y+j] != 0 && Map.wall[tank_x+i][tank_y+j] != 0 && Map.water[tank_x+i][tank_y+j] != 0)
-						return false;
-				}
+			for (int i = 0; i < 5; i++) {
+				a = tank_y - 1;
+				if (Map.ironwall[tank_x + i][a] != 0 && Map.wall[tank_x + i][a] != 0 && Map.water[tank_x + i][a] != 0)
+					return false;
 			}
+
 			return true;
 
 		case DOWN:
-			for(int i=0;i<5;i++){
-				for(int j=0;j<5;j++) {
-					if(Map.ironwall[tank_x+i][tank_y+j] != 0 && Map.wall[tank_x+i][tank_y+j] != 0 && Map.water[tank_x+i][tank_y+j] != 0)
-						return false;
-				}
+			for (int i = 0; i < 5; i++) {
+				a = tank_y + 1;
+				if (Map.ironwall[tank_x + i][a] != 0 && Map.wall[tank_x + i][a] != 0 && Map.water[tank_x + i][a] != 0)
+					return false;
 			}
+
 			return true;
-			
+
 		case LEFT:
-			for(int i=0;i<5;i++){
-				for(int j=0;j<5;j++) {
-					if(Map.ironwall[tank_x+i][tank_y+j] != 0 && Map.wall[tank_x+i][tank_y+j] != 0 && Map.water[tank_x+i][tank_y+j] != 0)
-						return false;
-				}
+			for (int i = 0; i < 5; i++) {
+				a = tank_x - 1;
+				if (Map.ironwall[a][tank_y + i] != 0 && Map.wall[a][tank_y + i] != 0 && Map.water[a][tank_y + i] != 0)
+					return false;
 			}
+
 			return true;
-			
+
 		case RIGHT:
-			for(int i=0;i<5;i++){
-				for(int j=0;j<5;j++) {
-					if(Map.ironwall[tank_x+i][tank_y+j] != 0 && Map.wall[tank_x+i][tank_y+j] != 0 && Map.water[tank_x+i][tank_y+j] != 0)
-						return false;
-				}
+			for (int i = 0; i < 5; i++) {
+				a = tank_x + 1;
+				if (Map.ironwall[a][tank_y + i] != 0 && Map.wall[a][tank_y + i] != 0 && Map.water[a][tank_y + i] != 0)
+					return false;
 			}
+
 			return true;
-			
+
 		default:
 			return false;
 
@@ -120,9 +100,6 @@ public class AiTank extends Tank implements Movable {
 
 	}
 
-	/**
-	 * ���
-	 */
 	@Override
 	public Bullet shootBullet() {
 		int x = this.tank_x + 2;
@@ -132,33 +109,29 @@ public class AiTank extends Tank implements Movable {
 		return new Bullet(x, y, direction);
 	}
 
-	// FIXME 你介个应该放下面方法体里面吧
-	private boolean f = false;
-
-	/**
-	 * Ϊ�з�̹�������ƶ��ķ���
-	 */
 	private void setRandomDir() {
+		boolean f = false;
+
 		while (f = false) {
 			int r = new Random().nextInt(5);
 			switch (r) {
 			case 1: {
-				direction = 1;
+				direction = UP;
 				f = true;
 				break;
 			}
 			case 2: {
-				direction = 2;
+				direction = DOWN;
 				f = true;
 				break;
 			}
 			case 3: {
-				direction = 3;
+				direction = LEFT;
 				f = true;
 				break;
 			}
 			case 4: {
-				direction = 4;
+				direction = RIGHT;
 				f = true;
 				break;
 			}
@@ -171,10 +144,10 @@ public class AiTank extends Tank implements Movable {
 		}
 	}
 
-	public static final int UP = 1; // ����
-	public static final int DOWN = 2; // ����
-	public static final int LEFT = 3; // ����
-	public static final int RIGHT = 4; // ����
+	public static final int UP = 1;
+	public static final int DOWN = 2;
+	public static final int LEFT = 3;
+	public static final int RIGHT = 4;
 
 	public AiTank(int tank_x, int tank_y, int direction, int HP) {
 
@@ -187,50 +160,26 @@ public class AiTank extends Tank implements Movable {
 
 	}
 
-	/**
-	 * ����̹��x����
-	 * 
-	 * @return
-	 */
 	public int getTank_x() {
 		return tank_x;
 	}
 
-	/**
-	 * ����̹��y����
-	 * 
-	 * @return
-	 */
 	public int getTank_y() {
 		return tank_y;
 	}
 
-	/**
-	 * ����hpֵ
-	 * 
-	 * @return
-	 */
 	public int getHP() {
 		return HP;
 	}
 
-	/**
-	 * ���ظ�̹�˳�ʼx����
-	 */
 	public int getInitX() {
 		return this.initX;
 	}
 
-	/**
-	 * ���ظ�̹�˵ĳ�ʼy����
-	 */
 	public int getInitY() {
 		return this.initY;
 	}
 
-	/**
-	 * ��������Ѫ��ֵ
-	 */
 	public void setHP() {
 		this.HP = 3;
 	}
@@ -249,22 +198,6 @@ public class AiTank extends Tank implements Movable {
 
 	public void setInitY(int y) {
 		this.initY = y;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public void setWidth(int width) {
-		this.width = width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
 	}
 
 }
