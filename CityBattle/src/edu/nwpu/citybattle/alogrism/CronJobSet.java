b6e5330e501efd6023de53e00372a70fe16327c3
Orangii <14857984+Jiaocz@users.noreply.github.com>
2020-12-29 22:37:39 +0800

    新增延时执行任务。

--- CityBattle/src/edu/nwpu/citybattle/alogrism/CronJobSet.java
+++ CityBattle/src/edu/nwpu/citybattle/alogrism/CronJobSet.java
@@ -15,6 +15,7 @@ import java.util.Timer;
  * {@code Movable}及{@code CronJob}接口均可以挂在到本计时器下，实现任务的自动执行。<br />
  * 计时器的最小执行时间是游戏设定的刷新率，因为这个值没有静态类保存，在这里我定义为30FPS，<br />
  * 最小执行间隔时间为 1/30s ，小于改值的任务也会使用此时间间隔作为任务间的间隔。<br />
+ * 目前也支持延时执行任务，一次性即失效，请看{@code addDelayJob(Runnable)}<br />
  * <hr />
  * 最简单的，您可以直接将您的{@code Movable}接口使用{@code addJob(Movable)}方法添加进本定时器，本定时器将在每帧执行这些任务，<br
  * />
@@ -37,7 +38,7 @@ import java.util.Timer;
  * <hr />
  * 
  * @author Orangii
- * @version 1.1.2
+ * @version 1.2.0
  * @see Runnable
  * @see CronJob
  * @see edu.nwpu.citybattle.actions.Movable
@@ -57,6 +58,11 @@ public final class CronJobSet extends TimerTask {
 	// 上次执行时间
 	private static ArrayList<Long> CronJobsLast = new ArrayList<Long>();
 	private static ArrayList<Long> MovesLast = new ArrayList<Long>();
+	
+	// 延时执行定时器
+	private static ArrayList<Runnable> DelayJob = new ArrayList<Runnable>();
+	private static ArrayList<Long> DelayJobAddTime = new ArrayList<Long>();
+	private static ArrayList<Long> DelayJobDelays = new ArrayList<Long>();
 
 	// 定时器本体
 	private static Timer timer = new Timer();
@@ -159,6 +165,26 @@ public final class CronJobSet extends TimerTask {
 		}
 		return status;
 	}
+	
+	/**
+	 * 添加延时执行任务，可以使用匿名内部类或者实现{@code Runnable}或{@code CronJob}接口添加到本类。<br />
+	 * 延时任务仅执行一次，一次后失效。<br />
+	 * 目前延迟任务不支持删除。
+	 * 
+	 * @since 1.2.0
+	 * @param Job	需要延时运行的任务
+	 * @param Delay	延迟时间
+	 * @return	是否成功添加任务
+	 */
+	public static boolean addDelayJob(Runnable Job, Long Delay) {
+		if(DelayJob.add(Job)) {
+			DelayJobAddTime.add(DelayJob.indexOf(Job), System.currentTimeMillis());
+			DelayJobDelays.add(DelayJob.indexOf(Job), Delay);
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 	/**
 	 * 通过指定任务删除一个定时任务。
@@ -204,6 +230,7 @@ public final class CronJobSet extends TimerTask {
 	public void run() {
 		runCronJobs();
 		runMovables();
+		runDelays();
 	}
 
 	// CronJob接口下的任务
@@ -222,6 +249,19 @@ public final class CronJobSet extends TimerTask {
 			if (System.currentTimeMillis() - MovesLast.get(i) >= FreshRate) {
 				MovesLast.set(i, System.currentTimeMillis());
 				Moves.get(i).moveNext();
+			}
+		}
+	}
+	
+	// 延时任务执行
+	private static void runDelays() {
+		for(int i = 0; i < DelayJob.size(); i++) {
+			if(System.currentTimeMillis() - DelayJobAddTime.get(i) >= DelayJobDelays.get(i)) {
+				DelayJob.get(i).run();
+				
+				DelayJob.remove(i);
+				DelayJobAddTime.remove(i);
+				DelayJobDelays.remove(i);
 			}
 		}
 	}
