edf183b02f6b60828e8f05e00a255fa2915324de
Orangii <14857984+Jiaocz@users.noreply.github.com>
2020-12-30 19:22:11 +0800

    多线程类

--- CityBattle/src/edu/nwpu/citybattle/alogrism/ThreadCronJob.java
+++ CityBattle/src/edu/nwpu/citybattle/alogrism/ThreadCronJob.java
@@ -13,9 +13,9 @@ import edu.nwpu.citybattle.actions.Movable;
  * @author Orangii
  *
  */
-public class ThreadCronJob extends CronJobSet {
-	
-	static class BulletThread implements Runnable {
+public final class ThreadCronJob extends CronJobSet {
+	// 子弹线程存储类
+	private static class BulletThread implements Runnable {
 		public static ArrayList<Movable> bullets = new ArrayList<Movable>();
 		@Override
 		public void run() {
@@ -29,7 +29,8 @@ public class ThreadCronJob extends CronJobSet {
 			}
 		}
 	}
-	static class TankThread implements Runnable {
+	// 坦克线程存储类
+	private static class TankThread implements Runnable {
 		public static ArrayList<Movable> tanks = new ArrayList<Movable>();
 		public static ArrayList<Long> tanks_last = new ArrayList<Long>();
 		@Override
@@ -47,7 +48,30 @@ public class ThreadCronJob extends CronJobSet {
 			}
 		}
 	}
+	// 地图刷新线程
+	private static class MapThread implements Runnable {
+		public static Runnable map;
+		public static Long lastTime;
+		@Override
+		public void run() {
+			if(System.currentTimeMillis() - lastTime >= FreshRate) {
+				lastTime = System.currentTimeMillis();
+				map.run();
+			}
+			try {
+				Thread.sleep(FreshRate);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
+	/**
+	 * 添加坦克或子弹类线程，嗯对直接添加就可以
+	 * 
+	 * @param cronJob	坦克或子弹对象
+	 * @return	是否添加成功
+	 */
 	public static boolean addJob(Movable cronJob) {
 		if(cronJob instanceof Bullet) {
 			return BulletThread.bullets.add(cronJob);
@@ -59,18 +83,34 @@ public class ThreadCronJob extends CronJobSet {
 			}
 			else return false;
 		}
-		else {
-			return false;
-		}
+		else return false;
+	}
+	/**
+	 * 添加地图刷新线程，传入{@code Runnable}接口匿名内部类或者实现{@code Runnable}或{@code CronJob}接口
+	 * 
+	 * @param cronJob	地图执行方法
+	 * @return	添加是否成功（实际上并不会失败）
+	 */
+	public static boolean addJob(Runnable cronJob) {
+		MapThread.map = cronJob;
+		MapThread.lastTime = System.currentTimeMillis();
+		return true;
 	}
 	
-	static BulletThread bulletT = new BulletThread();
-	static TankThread tankT = new TankThread();
-	static Thread bullet = new Thread(bulletT,"Bullet");
-	static Thread tank = new Thread(tankT,"Bullet");
-	static boolean bullet_started = false;
-	static boolean tank_started = false;
+	// 三个线程
+	private static Thread bullet = new Thread(new BulletThread(),"Bullet");
+	private static Thread tank = new Thread(new TankThread(),"Bullet");
+	private static Thread map = new Thread(new MapThread(),"Map");
+	// 线程是否已经启动
+	private static boolean bullet_started = false;
+	private static boolean tank_started = false;
+	private static boolean map_started = false;
 	
+	/**
+	 * 启动三个线程，已经启动的并不会启动。
+	 * 
+	 * @since 1.0.0
+	 */
 	public static void start() {
 		if(!bullet_started) {
 			bullet.start();
@@ -82,7 +122,32 @@ public class ThreadCronJob extends CronJobSet {
 			tank_started = true;
 		}
 		
+		if(!map_started) {
+			map.start();
+			map_started = true;
+		}
+		
 	}
 	
+	/**
+	 * 强制停止三个线程，可能造成安全问题。
+	 * 
+	 * @since 1.0.0
+	 * @deprecated
+	 */
+	public static void stop() {
+		if(bullet_started) {
+			bullet.stop();
+			bullet_started = false;
+		}
+		if(tank_started) {
+			tank.stop();
+			tank_started = false;
+		}
+		if(map_started) {
+			map.stop();
+			map_started = false;
+		}
+	}
 	
 }
