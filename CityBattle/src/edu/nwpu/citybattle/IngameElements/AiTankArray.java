4ea7a14df78efc5eea8fd67668a8ade25d021a49
Aurora <Aurora@LX20201226>
2021-01-09 15:16:19 +0800

    按地图绘制AiTank

--- CityBattle/src/edu/nwpu/citybattle/IngameElements/AiTankArray.java
+++ CityBattle/src/edu/nwpu/citybattle/IngameElements/AiTankArray.java
@@ -19,11 +19,37 @@ public class AiTankArray {
 	public static final ArrayList<AiTank> aiTank = new ArrayList<AiTank>();
 
 	/**
-	 * 如果检测到有死亡的坦克，则在此基础上在该坦克的起始位置重新生成一个坦克
+	 *按地图绘制AiTank
 	 */
 	public void choice() {
 		if(Choice.mapNumber == 1) {
-			new AiTank(1,30,Tank.RIGHT,3);
+			new AiTank(1,30,Tank.DOWN,3);
+			new AiTank(35,1,Tank.RIGHT,3);
+			new AiTank(1,45,Tank.DOWN,3);
 		}
+		
+		if(Choice.mapNumber == 2) {
+			new AiTank(20,1,Tank.RIGHT,3);
+			new AiTank(50,1,Tank.DOWN,3);
+			new AiTank(50,29,Tank.DOWN,3);
+			new AiTank(1,30,Tank.DOWN,3);
+		}
+		
+		if(Choice.mapNumber == 3) {
+			new AiTank(1,1,Tank.DOWN,3);
+			new AiTank(50,1,Tank.RIGHT,3);
+			new AiTank(1,20,Tank.DOWN,3);
+			new AiTank(1,27,Tank.DOWN,3);
+			new AiTank(50,35,Tank.DOWN,3);
+		}
+		
+		if(Choice.mapNumber == 4) {
+			new AiTank(1,1,Tank.RIGHT,3);
+			new AiTank(40,1,Tank.RIGHT,3);
+			new AiTank(1,27,Tank.DOWN,3);
+			new AiTank(52,18,Tank.DOWN,3);
+			new AiTank(13,8,Tank.DOWN,3);
+		}
+	
 	}
 }
