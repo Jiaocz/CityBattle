28180235956cca9034dc19edd7ef3c849b6ec0eb
Aurora <Aurora@LX20201226>
2021-01-09 14:54:28 +0800

    边界判定条件

--- CityBattle/src/edu/nwpu/citybattle/IngameElements/AiTank.java
+++ CityBattle/src/edu/nwpu/citybattle/IngameElements/AiTank.java
@@ -72,9 +72,7 @@ public class AiTank extends Tank implements Movable {
 			}
 		} else
 			setRandomDir();
-	/**
-	 * 判断AiTank现在的方向做出判断
-	 */
+	
 	switch(this.HP) {
 		case 1:
 			if(this.direction == Tank.UP) {
@@ -172,7 +170,7 @@ public class AiTank extends Tank implements Movable {
 		case Tank.UP:
 			for (int i = 0; i < 3; i++) {
 				a = tank_y - 1;
-				if (Map.ironwall[tank_x + i][a] != 0 && Map.wall[tank_x + i][a] != 0 && Map.water[tank_x + i][a] != 0)
+				if (Map.ironwall[tank_x + i][a] != 0 || Map.wall[tank_x + i][a] != 0 || Map.water[tank_x + i][a] != 0)
 					return false;
 			}
 
@@ -181,7 +179,7 @@ public class AiTank extends Tank implements Movable {
 		case Tank.DOWN:
 			for (int i = 0; i < 3; i++) {
 				a = tank_y + 1;
-				if (Map.ironwall[tank_x + i][a] != 0 && Map.wall[tank_x + i][a] != 0 && Map.water[tank_x + i][a] != 0)
+				if (Map.ironwall[tank_x + i][a] != 0 || Map.wall[tank_x + i][a] != 0 || Map.water[tank_x + i][a] != 0)
 					return false;
 			}
 
@@ -190,7 +188,7 @@ public class AiTank extends Tank implements Movable {
 		case Tank.LEFT:
 			for (int i = 0; i < 3; i++) {
 				a = tank_x - 1;
-				if (Map.ironwall[a][tank_y + i] != 0 && Map.wall[a][tank_y + i] != 0 && Map.water[a][tank_y + i] != 0)
+				if (Map.ironwall[a][tank_y + i] != 0 || Map.wall[a][tank_y + i] != 0 || Map.water[a][tank_y + i] != 0)
 					return false;
 			}
 
@@ -199,7 +197,7 @@ public class AiTank extends Tank implements Movable {
 		case Tank.RIGHT:
 			for (int i = 0; i < 3; i++) {
 				a = tank_x + 1;
-				if (Map.ironwall[a][tank_y + i] != 0 && Map.wall[a][tank_y + i] != 0 && Map.water[a][tank_y + i] != 0)
+				if (Map.ironwall[a][tank_y + i] != 0 || Map.wall[a][tank_y + i] != 0 || Map.water[a][tank_y + i] != 0)
 					return false;
 			}
 
